8ce1a13977845546858c7707d69e7a2f4f83fe6b
Jesse Vista <codegreencreative@users.noreply.github.com>
2014-09-27 09:36:22 -0400

    Update MemcacheHandler.php

--- src/Igormatkovic/Memcache/MemcacheHandler.php
+++ src/Igormatkovic/Memcache/MemcacheHandler.php
@@ -11,7 +11,7 @@ class MemcacheHandler implements \SessionHandlerInterface {
      * @param  int  $minutes
      * @return void
      */
-    public function __construct(Repository $cache, $minutes)
+    public function __construct(\Repository $cache, $minutes)
     {
         $this->cache = $cache;
         $this->minutes = $minutes;
