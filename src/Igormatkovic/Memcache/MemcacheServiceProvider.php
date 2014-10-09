3b6d82dc80e2d9aa54faed318d3d2d2231b313c7
Andrew Welters <awelters@hugmehugyou.org>
2014-10-09 14:18:19 -0500

    Update MemcacheServiceProvider.php

--- src/Igormatkovic/Memcache/MemcacheServiceProvider.php
+++ src/Igormatkovic/Memcache/MemcacheServiceProvider.php
@@ -8,11 +8,11 @@ use Illuminate\Cache\CacheManager;
 class MemcacheServiceProvider extends ServiceProvider {
 
     /**
-   	 * Indicates if loading of the provider is deferred.
+        * Indicates if loading of the provider is deferred.
      *
      * @var bool
      */
-	protected $defer = false;
+    protected $defer = false;
 
     /**
      * Register the service provider.
@@ -21,31 +21,39 @@ class MemcacheServiceProvider extends ServiceProvider {
      */
     public function register()
     {
-     	$servers = $this->app['config']['cache.memcached'];
+        $isCacheDriver = $this->app['config']['cache.driver'] == 'memcache';
+        $servers = $this->app['config']['cache.memcached'];
         $prefix = $this->app['config']['cache.prefix'];
+        $isSessionDriver = $this->app['config']['session.driver'] == 'memcache';
         $minutes = $this->app['config']['session.lifetime'];
+        $memcache = $repo = $handler = $manager = $driver = null;
 
-        $memcache = new MemcacheConnector();
-        $memcache = $memcache->connect($servers);
-        $repo = new Repository(new MemcacheStore($memcache, $prefix));
-        $handler = new MemcacheHandler($repo, $minutes);
+        if($isCacheDriver) {
+            $memcache = new MemcacheConnector();
+            $memcache = $memcache->connect($servers);
+            $repo = new Repository(new MemcacheStore($memcache, $prefix));
 
-        $this->app->resolving('cache', function($cache) use ($repo)
-        {
-            $cache->extend('memcache', function($app) use ($repo)
+            $this->app->resolving('cache', function($cache) use ($repo)
             {
-             	return $repo;
+                $cache->extend('memcache', function($app) use ($repo)
+                {
+                     return $repo;
+                });
             });
-        });
 
-        $this->app->resolving('session', function($session) use ($handler)
-    	{
-            $session->extend('memcache', function($app) use ($handler)
-            {
+            if($isSessionDriver) {
+                $handler = new MemcacheHandler($repo, $minutes);
                 $manager = new MemcacheSessionManager($handler);
+                $driver = $manager->driver('memcache');
 
-                return $manager->driver('memcache');
-            });
-        });
+                $this->app->resolving('session', function($session) use ($driver)
+                {
+                    $session->extend('memcache', function($app) use ($driver)
+                    {
+                        return $driver;
+                    });
+                });
+            }
+        }
     }
 }
